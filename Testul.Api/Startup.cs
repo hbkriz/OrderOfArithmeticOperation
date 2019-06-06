f8f041362d8120a4f11cf718cd1d9e7c5c6ee235
Hari <hari.balakrishnan@willmottdixon.co.uk>
2019-06-06 12:05:24 +0100

    Setup

---
+++ Testul.Api/Startup.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace Testul.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSwaggerGen(action =>
+                action.SwaggerDoc(Configuration["SwaggerInfo:Version"],
+                    new Info
+                    {
+                        Title = Configuration["SwaggerInfo:ApplicationName"],
+                        Version = Configuration["SwaggerInfo:Version"]
+                    }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(action =>
+            {
+                action.RoutePrefix = "";
+                action.SwaggerEndpoint($"/swagger/{Configuration["SwaggerInfo:Version"]}/swagger.json",
+                    Configuration["SwaggerInfo:ApplicationName"]);
+                action.DocExpansion(DocExpansion.None);
+            });
+        }
+    }
+}
